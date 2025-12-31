a5911d3e4e30d5467dd424a04b485530cadc4190
Python-Projects <abishekchamlagain@gmail.com>
2025-12-31 20:45:26 +0545

    add random password generator

--- random_password_generator.py
+++ random_password_generator.py
@@ -1,11 +1,24 @@
 import random
 import string
 
-user_input=int(input("Enter length for password"))
-print(string.ascii_letters)
-print(string.digits)
-print(string.punctuation)
+allchar=string.ascii_letters + string.digits + string.punctuation
 
+while True:
+    try:
+          user_input=int(input("Enter length for password: "))
+    except ValueError:
+            print("Please enter a valid number")
+            continue
+    
+    if user_input <= 0:
+        print("Password length must be greater than 0")
+        continue
 
+    random_password = ""
+    for i in range(user_input):
+          random_pass = random.choices(allchar)
+          random_password = random_pass + random_pass
+          
+    print("Random password is: ",random_password)
+    break
 
-# rand-password=random.choices()
