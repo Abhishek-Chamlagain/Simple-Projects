9e5d3a33485d7610a51db982cdafcdcd487128df
Python-Projects <abishekchamlagain@gmail.com>
2025-12-31 19:20:56 +0545

    add random password generator

---
+++ random_password_generator.py
@@ -0,0 +1,11 @@
+import random
+import string
+
+user_input=int(input("Enter length for password"))
+print(string.ascii_letters)
+print(string.digits)
+print(string.punctuation)
+
+
+
+# rand-password=random.choices()
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+rand-password=random.choices()
